f494d987c012c9a0af5ae501a73157aa20bf7a92
Renard <rdlafitte@gmail.com>
2021-04-14 19:55:57 -0300

    Clase Lata
    se agrega la clase lata

---
+++ Expendedora/Expendedora.Libreria/Entidades/Lata.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expendedora.Libreria.Entidades
+{
+
+       public class Lata
+        {
+            private string _codigo;
+            private string _nombre;
+            private string _sabor;
+            private double _precio;
+            private double _volumen;
+            private int _cantidad;
+
+            public string Codigo { get => _codigo; set => _codigo = value; }
+            public string Nombre { get => _nombre; set => _nombre = value; }
+            public string Sabor { get => _sabor; set => _sabor = value; }
+            public double Precio { get => _precio; set => _precio = value; }
+            public double Volumen { get => _volumen; set => _volumen = value; }
+            public int Cantidad { get => _cantidad; set => _cantidad = value; }
+
+            public Lata()
+            {
+
+            }
+            public Lata(string codigo)
+            {
+                _codigo = codigo;
+            }
+            public Lata(string codigo, string nombre, string sabor, double precio, double volumen, int cantidad)
+            {
+                _codigo = codigo;
+                _nombre = nombre;
+                _sabor = sabor;
+                _precio = precio;
+                _volumen = volumen;
+                _cantidad = cantidad;
+            }   
+        }
+    }
+
