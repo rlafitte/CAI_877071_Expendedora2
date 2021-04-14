47c22c7cabceb6255d4886fa29d9556535b754cc
Renard <rdlafitte@gmail.com>
2021-04-14 19:53:34 -0300

    Inicio programa

---
+++ Expendedora/Expendedora.Libreria/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expendedora.Libreria
+{
+    public class Class1
+    {
+    }
+}
